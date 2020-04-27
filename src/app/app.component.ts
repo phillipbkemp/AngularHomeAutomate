c4ba230702be84ae85026250b9cae628fa720c29
Phillip Kemp <phillip-kemp@softwareimaging.com>
2020-04-27 09:09:57 +0100

    MVP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { environment } from './../environments/environment';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-home-automate';
+
+  private url = environment.api_url; 
+  private _http: HttpClient;
+  private _response: object;
+  
+  constructor(http: HttpClient) {
+    this._http = http;
+  }
+
+  getPosts(ref: number, label: string) {
+    let uri: string = `${this.url}?request=controldevicebylabel&ref=${ref}&label=${label}`;
+    this._http.get(uri)
+    .subscribe(
+    response => this._response = response);
+    }
+
+
 }
