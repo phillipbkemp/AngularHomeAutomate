9803a429328d7e5ed3c0078b2e909a0d197afacb
RedPeril <phillip@phillipkemp.com>
2022-10-09 19:19:31 +0100

    Move to Hubitat Sideboard control

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent {
   title = 'angular-home-automate';
   private url = 'http://server/JSON'
+  private hubitatUrl = 'http://hubitat/apps/api/261/devices'
   private _http: HttpClient;
   private _response: object;
   
@@ -24,5 +25,11 @@ export class AppComponent {
     response => this._response = response);
     }
 
+  controlHubitatSwitch(deviceId: number, command: string) {
+    let uri: string = `${this.hubitatUrl}/${deviceId}/${command}?access_token=527a2413-1325-4282-99db-e414ef25ef4c`;
+    this._http.get(uri)
+    .subscribe(
+    response => this._response = response);
+    }
 
 }
